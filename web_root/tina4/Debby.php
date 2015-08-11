159df8df149ab917016b47805f944194337a5883
Andre van Zuydam <andrevanzuydam@gmail.com>
2015-08-11 14:27:08 +0200

    Crypt changed to password hash

--- web_root/tina4/Debby.php
+++ web_root/tina4/Debby.php
@@ -2706,7 +2706,7 @@ Ruth::setOBJECT("'.Ruth::getREQUEST("txtALIAS").'", $'.Ruth::getREQUEST("txtALIA
 
                 foreach ($tempfields as $id => $fieldname) { //Look for password fields
                     if (trim(strtoupper($name)) === trim(strtoupper($fieldprefix . $fieldname))) {
-                        $value = crypt($value);
+                        $value = password_hash($value, PASSWORD_DEFAULT);
                     }
                 }
                 $tempfields = explode(",", $datefields);
@@ -2827,7 +2827,7 @@ Ruth::setOBJECT("'.Ruth::getREQUEST("txtALIAS").'", $'.Ruth::getREQUEST("txtALIA
                 foreach ($tempfields as $id => $fieldname) { //Look for password fields
                     if ($name == $fieldprefix . strtoupper($fieldname)) {
                         if ($value != "") { //only if there is a password do we encrypt it
-                            $value = crypt($value);
+                            $value = password_hash($value, PASSWORD_DEFAULT);
                         } else {
                             $dontupdate = true; //we must not update an empty password
                         }
@@ -2900,9 +2900,7 @@ Ruth::setOBJECT("'.Ruth::getREQUEST("txtALIAS").'", $'.Ruth::getREQUEST("txtALIA
             fputcsv($fh, array_map(function($element) { return $element['alias']; }, $this->fieldinfo), $delim); 
             
             foreach($results as $result){
-                 
-                fputcsv($fh, $result, $delim); 
-
+                fputcsv($fh, $result, $delim);
             }
             
             fclose($fh);

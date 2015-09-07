31d2126703bdf3d948334f5386ef6f26db2561ef
Andre van Zuydam <andrevanzuydam@gmail.com>
2015-09-07 14:22:34 +0200

    Olga is now recursive and object wrapper for array of objects

--- web_root/tina4/Olga.php
+++ web_root/tina4/Olga.php
@@ -6,14 +6,60 @@
  * Date: 2015-09-04
  * Time: 04:05 PM
  */
-class Olga {
+class Olga implements Iterator  {
+
+    var $arrayObjects = []; //an array or collection of objects of the same type as this (we probably need to check if it is the same)
+
+    /**
+     * The clone function must recreate the object closures
+     */
+    function __clone() {
+        //we will have to clone the closures
+        foreach( (array) $this as $key => $value ) {
+            if (is_callable($this->$key)) {
+                if (substr($key,0,3) === "get") {
+                    eval ('
+                    $this->' . $key . ' = function () {
+                        return $this->' . strtolower($key[3]).substr($key,4) . ';
+                    };');
+                }
+
+                if (substr($key,0,3) === "set") {
+                    eval ('
+                    $this->' . $key . ' = function ($value) {
+                         $this->' . strtolower($key[3]).substr($key,4) . ' = $value;
+                    };');
+                }
+
+            }
+        }
+    }
+
+    /**
+     * Append an object that has similar properties as the known object
+     */
+    function append($object) {
+        if (is_object($object)) {
+
+            $this->arrayObjects[] = clone $object;
+        }
+        else {
+            throw new Exception("Olga says this is not an object");
+        }
+    }
+
 
     /**
      * Create a JSON instance of the object
      * @return string
      */
     function toJSON() {
-        return $this->__toJson($this);
+        //print_r ((array) $this);
+        if (count($this->arrayObjects) > 0) {
+            return $this->__toJson(  $this->arrayObjects);
+        } else {
+            return $this->__toJson((array)$this);
+        }
     }
 
     /**
@@ -35,19 +81,34 @@ class Olga {
     function __toJson ( $data ) {
         if( is_array($data) || is_object($data) ) {
             $islist = is_array($data) && ( empty($data) || array_keys($data) === range(0,count($data)-1) );
-
             if( $islist ) {
-                $json = '[' . implode(',', array_map('__toJson', $data) ) . ']';
+                $json = '[' . join(',', array_map([$this, '__toJson'],  $data) ) . ']';
             } else {
                 $items = Array();
-                foreach( $data as $key => $value ) {
-                    if (!is_callable($this->$key)) {
-                        $items[] = $this->__toJson("$key") . ':' . $this->__toJson($value);
+
+                if (is_object($data)) {
+                    $data = (array) $data;
+                }
+
+                foreach(  $data as $key => $value ) {
+                    $isClosure = false;
+
+                    if (is_object($value) && get_class($value) === "Closure") {
+                        $isClosure = true;
+                    }
+
+
+
+                    if (!$isClosure && $key !== "arrayObjects") {
+
+                        $items[] = $this->__toJson("$key") . ':' . $this->__toJson( $value);
                     }
                 }
+
                 $json = '{' . implode(',', $items) . '}';
             }
         } elseif( is_string($data) ) {
+
             # Escape non-printable or Non-ASCII characters.
             # I also put the \\ character first, as suggested in comments on the 'addclashes' page.
             $string = '"' . addcslashes($data, "\\\"\n\r\t/" . chr(8) . chr(12)) . '"';
@@ -91,6 +152,7 @@ class Olga {
             }
         } else {
             # int, floats, bools, null
+
             $json = strtolower(var_export( $data, true ));
         }
         return $json;
@@ -111,6 +173,11 @@ class Olga {
         return false;
     }
 
+    //Save the object to disk or database or xcache in a structure
+    function persist() {
+        return false;
+    }
+
     /**
      * Constructor to make getters and setters
      */
@@ -121,18 +188,79 @@ class Olga {
 
         $variables = get_object_vars($this);
         foreach ($variables as $varName => $varValue) {
-            $getter = "get".UcWords($varName);
-            $setter = "set".UcWords($varName);
-            eval ('
-            $this->'.$getter.' = function () {
-                return $this->'.$varName.';
-            };
+            if ($varName != "arrayObjects") {
+                $getter = "get" . UcWords($varName);
+                $setter = "set" . UcWords($varName);
+                eval ('
+                    $this->' . $getter . ' = function () {
+                        return $this->' . $varName . ';
+                    };
 
-            $this->'.$setter.' = function ($value) {
-                $this->'.$varName.' = $value;
-            };
-            ');
+                    $this->' . $setter . ' = function ($value) {
+                        $this->' . $varName . ' = $value;
+                        $this->persist();
+                    };
+                    ');
+            }
 
         }
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->arrayObjects);
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        return next ($this->arrayObjects);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->arrayObjects);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        $key = key($this->arrayObjects);
+        $var = ($key !== NULL && $key !== FALSE);
+        return $var;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->arrayObjects);
     }
 }
